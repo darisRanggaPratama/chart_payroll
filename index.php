126fd52fee7e8ca72849bcd07809db1e8e0ac3c8
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-06-26 06:22:26 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payroll Chart</title>
+</head>
+
+<body>
+    <object data="chart_bruto_trf.php" height="1015px" width="100%">
+        Your browser does not support the object tag.
+    </object>
+
+    <object data="table.php" height="820px" width="100%">
+        Your browser does not support the object tag.
+    </object>
+
+    <object data="chart_gp_pot.php" height="1015px" width="100%">
+        Your browser does not support the object tag.
+    </object>
+    <object data="chart_lembur_tjlain.php" height="1015px" width="100%">
+        Your browser does not support the object tag.
+    </object>
+    <object data="chart_human.php" height="1015px" width="100%">
+        Your browser does not support the object tag.
+    </object>
+</body>
+
+</html>
