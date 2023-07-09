c88e0b4d06f9c6680d317adcb9b6f5135bd1a3a6
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-07-09 14:50:30 +0700

    initial commit

--- dummy/logout.php
+++ dummy/logout.php
@@ -4,6 +4,6 @@ session_start();
 
 session_destroy();
 
-header("location:index.php?pesan=Anda berhasil logout.");
+header("location:index.php?pesan=Anda berhasil LOGOUT.");
 
 ?>
--- me-pst/main.php
+++ me-pst/main.php
@@ -14,7 +14,7 @@ if ($_SESSION['status'] != "sudah_login") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Malea Energy Pusat</title>
-    <link rel="stylesheet" href="css/style2.css" type="text/css">
+    <link rel="stylesheet" href="css/main.css" type="text/css">
 </head>
 
 <body>
